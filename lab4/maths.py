9e8667a89491e0e44a6760c126ff0ff442ef0b63
Asanalib <universekrovat@gmail.com>
2025-10-10 12:31:33 +0500

    lol

---
+++ lab4/maths.py
@@ -0,0 +1,26 @@
+import math
+
+# #1
+# a=int(input())
+# print(math.radians(a))
+
+# #2
+# a=int(input("\nHeight: "))
+# b=int(input("Base, first value: "))
+# c=int(input("Base, second value: "))
+# A=((c+b)/2)*a
+# print(A)
+
+# #3
+# a=int(input("\nInput number of sides: "))
+# b=int(input("Input the length of a side: "))
+
+# P=a*b
+# apo=b/(2*math.tan(180/a))
+# A=(apo*P)/2
+# print (A)
+
+#4
+a=int(input("\nLength of base: "))
+b=int(input("Height of parallelogram: "))
+print(a*b)
